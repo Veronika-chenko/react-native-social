b88954667bb476abe883e9de817fba6df1808116
Veronika Kravchenko <aniregma@gmail.com>
2023-03-23 23:16:46 +0200

    Remove the AsyncStorage warning
    Remove the AsyncStorage warning using @react-native-async-storage/async-storage package:
    used 'initializeAuth' and 'getReactNativePersistencemethods' instead of 'getAuth()' from firebase/auth

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,9 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from "firebase/auth";
+import {
+  initializeAuth,
+  getReactNativePersistence
+} from 'firebase/auth/react-native';
 
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
@@ -14,7 +18,10 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
 
 export const db = getFirestore(app);
 export const storage = getStorage(app);
